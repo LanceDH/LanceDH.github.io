4acb30ce2c99c0b78cc5b024a5bbcd0f0343a556
LanceDH <kiep_out@hotmail.com>
2018-05-03 17:01:09 +0200

    0.08
    Added navigation arrows to galleries

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,6 +5,24 @@ function GalleryToImage(id){
 	UpdateGallery(slideIndex = id);
 }
 
+function GalleryPrevImage(){
+	var images = document.getElementsByClassName("item");
+	slideIndex--;
+	if(slideIndex < 0){
+		slideIndex = images.length -1;
+	}
+	UpdateGallery(slideIndex);
+}
+
+function GalleryNextImage(){
+	var images = document.getElementsByClassName("item");
+	slideIndex++;
+	if(slideIndex >= images.length){
+		slideIndex = 0;
+	}
+	UpdateGallery(slideIndex);
+}
+
 function UpdateGallery(slideId){
 	var i;
 	var images = document.getElementsByClassName("item");
@@ -24,6 +42,24 @@ var devGalleryIndex = 0;
 UpdateDevGallery(devGalleryIndex);
 function DevGalleryToImage(id){
 	UpdateDevGallery(devGalleryIndex = id);
+}
+
+function DevGalleryPrevImage(){
+	var images = document.getElementsByClassName("item-dev");
+	devGalleryIndex--;
+	if(devGalleryIndex < 0){
+		devGalleryIndex = images.length -1;
+	}
+	UpdateDevGallery(devGalleryIndex);
+}
+
+function DevGalleryNextImage(){
+	var images = document.getElementsByClassName("item-dev");
+	devGalleryIndex++;
+	if(devGalleryIndex >= images.length){
+		devGalleryIndex = 0;
+	}
+	UpdateDevGallery(devGalleryIndex);
 }
 
 function UpdateDevGallery(slideId){
